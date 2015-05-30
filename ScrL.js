13cd58aa4dadb8df3d0e04ae4f94d7afa1fb503c
Saber Mogaddas <llsab3rll@gmail.com>
2015-05-30 11:26:55 +0430

    Weather22

---
+++ ScrL.js
@@ -0,0 +1,13 @@
+﻿
+
+$(document).ready(function () {
+
+			            var wSelect = $("#mySelect").val();
+			            $('#test').weatherfeed([wSelect]);
+			            $('#mySelect').change(function () {
+			               
+			                    $('.Loading-Txt').fadeIn(400).delay(50).fadeOut(200);
+			            
+			            $('#SVG').selector.reload(true);
+			            });
+			        });
